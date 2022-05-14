38b829c8819b84de3970ac748172dc1760807220
minnowo <minnowbrine@live.ca>
2022-05-14 00:33:10 -0400

    updated version number because I forgot about it last commit

--- src/minecraft/cute/Client.java
+++ src/minecraft/cute/Client.java
@@ -8,7 +8,7 @@ import cute.managers.ModuleManager;
 
 public class Client 
 {
-	public static final String VERSION = "0.1.0";
+	public static final String VERSION = "0.2.0";
 	public static final String NAME    = "Cute";
 	
 	private static final Client INSTANCE = new Client();
