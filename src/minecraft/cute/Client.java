e99c1511fda72592440941fddc641e105c93c7b2
minnowo <minnowbrine@live.ca>
2022-05-19 01:54:16 -0400

    version update

--- src/minecraft/cute/Client.java
+++ src/minecraft/cute/Client.java
@@ -11,7 +11,7 @@ import cute.managers.ModuleManager;
 
 public class Client 
 {
-	public static final String VERSION = "0.2.0";
+	public static final String VERSION = "0.3.0";
 	public static final String NAME    = "Cute";
 	
 //	public static final Logger LOGGER = LogManager.getLogger("Cute");
