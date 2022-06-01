20c9bd821f8649327fa688f73fa0411d018a9615
minnowo <minnowbrine@live.ca>
2022-05-31 20:17:04 -0400

    version

--- src/minecraft/cute/Client.java
+++ src/minecraft/cute/Client.java
@@ -15,7 +15,7 @@ import net.minecraft.item.ItemStack;
 
 public class Client 
 {
-	public static final String VERSION = "0.3.1";
+	public static final String VERSION = "0.4.0";
 	public static final String NAME    = "Cute";
 	
 	public static final Client INSTANCE = new Client();
