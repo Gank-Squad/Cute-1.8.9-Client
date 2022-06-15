bd9160e0b1a8fee3d818ad66e638619133c94e02
jasonmzx <68576119+jasonmzx@users.noreply.github.com>
2022-06-15 19:22:04 -0400

    bedprotect detecting sweet spot

--- src/minecraft/cute/modules/test/BedProtect.java
+++ src/minecraft/cute/modules/test/BedProtect.java
@@ -18,9 +18,9 @@ public class BedProtect extends Module {
 		super("Bed Protect", Category.BOT, "Protects your bed.");
 	}
 	
-	int[] bedPosition = new int[]{ -1698 , 76 , -96 };
+	int[] bedPosition = new int[]{ -1699 , 76 , -96 };
 	int width = 5;
-	int length = 5;
+	int length = 6;
 	
 	
 	private void keyWrap(String action, Boolean toggle ) 
@@ -64,7 +64,6 @@ public class BedProtect extends Module {
 		 Item.getItemById(5), //Wood
 		 Item.getItemById(20), //Glass
 		 Item.getItemById(35), //Wool
-		 Item.getItemById(4)
 	};
 	
 	int[] ItemIndices = new int[] 
@@ -72,63 +71,95 @@ public class BedProtect extends Module {
 		0, 1 , 2 , 3
 	};
 	
+	
+	int[][][] layout = {
+		{
+		{0,0,0,0,0},
+		{0,1,2,1,0},
+		{0,2,-1,2,0},
+		{0,2,-1,2,0},
+		{0,1,2,1,0},
+		{0,0,0,0,0},
+		},
+		{
+		{0,0,0,0,0},
+		{0,1,2,1,0},
+		{0,2,2,2,0},
+		{0,2,2,2,0},
+		{0,1,2,1,0},
+		{0,0,0,0,0},
+		},
+	};
+	
 	private static int _cooldownTicks = 0; 
-	private boolean sortCheck = false;
-
+	
+	private boolean buildSwitch = false;
+	
 	
 	@EventTarget
 	public void render(RenderWorldLastEvent e) 
 	{
+		boolean sortCheck = false;
 		InventoryPlayer inv = mc.thePlayer.getInventoryOfPlayer();
-			
 		
 		//Relative positions from bed [ X , Y , Z ]
-		int[] relPos = new int[] 
+		float[] relPos = new float[] 
 		{
-				(int) (bedPosition[0]-mc.thePlayer.posX),
-				(int) (bedPosition[1]-mc.thePlayer.posY),
-				(int) (bedPosition[2]-mc.thePlayer.posZ)
+				(float) (bedPosition[0]-mc.thePlayer.posX),
+				(float) (bedPosition[1]-mc.thePlayer.posY),
+				(float) (bedPosition[2]-mc.thePlayer.posZ)
 		};
 
-		if( sortCheck == false) {
-			for(int i = 0; i < ItemMaterials.length; i++)
-			{
-				int itemLocation = inv.getInventorySlotContainItem(ItemMaterials[i]);
-				
-				//Basically sending packets to the server:
-				mc.getMinecraft().playerController.windowClick(
-						mc.getMinecraft().thePlayer.inventoryContainer.windowId,
-						itemLocation, 0, 1, mc.getMinecraft().thePlayer);
+		System.out.println(buildSwitch);
+			
+		int midWidth = (int) Math.floor( (width) / 2 );
+		int midLength = (int) Math.ceil( (length+1) / 2 );
+		
+		
+		if(!buildSwitch) {
+			if(relPos[0] >= midLength && relPos[0] < midLength+1 
+					&&
+					relPos[2] >= midWidth && relPos[2] < midWidth+1) 
+					{
+						if( sortCheck == false) {
+							for(int i = 0; i < ItemMaterials.length; i++)
+							{
+								int itemLocation = inv.getInventorySlotContainItem(ItemMaterials[i]);
+								
+								//Basically sending packets to the server:
+								mc.getMinecraft().playerController.windowClick(
+										mc.getMinecraft().thePlayer.inventoryContainer.windowId,
+										itemLocation, 0, 1, mc.getMinecraft().thePlayer);
+							}
+							
+							sortCheck = true;
+							
+						}
+						buildSwitch = true;
+					}
+		}
+		
+		//Finding sweet spot
 
-//				ItemStack itemMeta =  inv.getInventorySlotContainItemStack(ItemMaterials[i]);			
-//				mc.thePlayer.replaceItemInInventory(itemLocation, Item.getItemById(0));
-			}
-			
-		}
 		
-			
-		
-		int midWidth = (width-1) / 2;
-		
-		
-		if( relPos[2] < ((midWidth-2)*-1) ) 
-		{
-			keyWrap("backward",true);
-			keyWrap("place",true);
-		}
-		
-		if(relPos[2] > midWidth+1)
-		{
-			keyWrap("place",false); //STOP Place blocks
-			keyWrap("backward",false); //STOP Move back
-		}
+//		if( relPos[2] < ((midWidth-2)*-1) ) 
+//		{
+//			keyWrap("backward",true);
+//			keyWrap("place",true);
+//		}
+//		
+//		if(relPos[2] > midWidth+1)
+//		{
+//			keyWrap("place",false); //STOP Place blocks
+//			keyWrap("backward",false); //STOP Move back
+//		}
 		
 
 		//System.out.println(mc.thePlayer.getHeldItem());
 		//mc.gameSettings.keyBindLeft.setPressed(true);
 		//mc.gameSettings.keyBindDrop.setPressed(true);  
-		mc.thePlayer.rotationYaw = 0F;
-		mc.thePlayer.rotationPitch = 45F;
+//		mc.thePlayer.rotationYaw = 0F;
+//		mc.thePlayer.rotationPitch = 45F;
 //		mc.thePlayer.replaceItemInInventory(1, STONE_itemstack );
 		//System.out.println("Target:");
 		//System.out.println(mc.thePlayer.getCurrentEquippedItem() );
@@ -136,7 +167,12 @@ public class BedProtect extends Module {
 		
 		//System.out.println("Get Item stack:");
 		//System.out.println(inv.getCurrentItem());
-		
+		System.out.println("X rel pos");
+		System.out.println(relPos[0]);
+		System.out.println(midWidth);
+		System.out.println("Z rel pos");
+		System.out.println(relPos[2]);
+		System.out.println(midLength);
 	}
 	
 }
